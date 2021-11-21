c479f115691eb9ad81418f224cd3edf675b62615
krutmaster <krutmaster25@mail.ru>
2021-11-21 16:38:06 +0300

    Fix send files

--- bot.py
+++ bot.py
@@ -8,22 +8,28 @@ import secret
 
 status_send = False
 count_got_files = 0
+boss = '538231919'
 bot = telebot.TeleBot(secret.token)
-bot.send_message(538231919, 'Бот перезапустился')
+bot.send_message(boss, 'Бот перезапустился')
 
 
 def send():
     global status_send
-    file_name = f'temp/{os.listdir("temp")[0]}'
     try:
+        files = os.listdir("temp")
+        if files[0] == 'readme.txt':
+            i = 1
+        else:
+            i = 0
+        file_name = f'temp/{os.listdir("temp")[i]}'
         with open(file_name, 'rb') as photo:
-            bot.send_photo(secret.channel, photo)
+            bot.send_photo(boss, photo)
         with open(file_name, 'rb') as photo:
-            bot.send_document(secret.channel, photo)
+            bot.send_document(boss, photo)
         os.remove(file_name)
         return True
     except Exception as e:
-        bot.send_message(538231919, f'send error\n\n{e}', disable_notification=True)
+        bot.send_message(boss, f'send error\n\n{e}', disable_notification=True)
         return False
 
 
@@ -31,43 +37,44 @@ def send():
 def switch(message):
     global status_send, count_got_files
     id = str(message.chat.id)
-    if id == '538231919':
+    if id == boss:
         status_send = True
         count_got_files = 0
         bot.send_message(id, 'Отправка включена')
 
 
 @bot.message_handler(commands=['count'])
-def switch(message):
+def count(message):
     id = str(message.chat.id)
-    if id == '538231919':
+    if id == boss:
         bot.send_message(id, count_got_files)
 
 
 def post_timer():
     global status_send
     while True:
+        print('yes')
         if status_send:
             files = os.listdir('temp')
-            if files:
-                result = False
+            if len(files) > 1:
+                result = send()
                 while not result:
                     time.sleep(300)
                     result = send()
-                bot.send_message(538231919, f'Фотка отправлена успешно, осталось {len(files)} фоток', disable_notification=True)
+                bot.send_message(boss, f'Фотка отправлена успешно, осталось {len(files) - 2} фоток', disable_notification=True)
                 time.sleep(7200)
             else:
                 status_send = False
-                bot.send_message(538231919, 'Фотки закончились')
+                bot.send_message(boss, 'Фотки закончились')
         else:
-            time.sleep(300)
+            time.sleep(5)
 
 
 @bot.message_handler(content_types=['document'])
 def handle_docs(message):
     global count_got_files
     id = str(message.chat.id)
-    if id == '538231919':
+    if id == boss:
         try:
             file_info = bot.get_file(message.document.file_id)
             downloaded_file = bot.download_file(file_info.file_path)
